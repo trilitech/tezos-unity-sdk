3a55c466f592ebc9f548836dd0a5a942ddf56c96
mismirnov <mi.smirnov24@gmail.com>
2023-03-06 21:26:36 +0300

    Added new dependencies, fixed newtonsoft and ios namespaces

--- Assets/Plugins/iOS/Editor/SwiftPostProcess.cs
+++ Assets/Plugins/iOS/Editor/SwiftPostProcess.cs
@@ -1,13 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+#if UNITY_IOS
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
-using System.Diagnostics;
-
-using System.IO;
-using System.Linq;
 
 // https://habibur-rahman-ovie.medium.com/unity-native-ios-plugin-bridge-between-swift-and-unity3d-d88861d0f456
 public static class SwiftPostProcess
@@ -15,31 +9,31 @@ public static class SwiftPostProcess
     [PostProcessBuild]
     public static void OnPostProcessBuild(BuildTarget buildTarget, string buildPath)
     {
-        if (buildTarget == BuildTarget.iOS)
-        {
-            var projPath = buildPath + "/Unity-Iphone.xcodeproj/project.pbxproj";
-            var proj = new PBXProject();
-            proj.ReadFromFile(projPath);
+        if (buildTarget != BuildTarget.iOS) return;
+        
+        var projPath = buildPath + "/Unity-Iphone.xcodeproj/project.pbxproj";
+        var proj = new PBXProject();
+        proj.ReadFromFile(projPath);
 
-            var targetGuid = proj.TargetGuidByName(PBXProject.GetUnityTestTargetName());
+        var targetGuid = proj.TargetGuidByName(PBXProject.GetUnityTestTargetName());
             
-            proj.SetBuildProperty(targetGuid, "ENABLE_BITCODE", "NO");
+        proj.SetBuildProperty(targetGuid, "ENABLE_BITCODE", "NO");
             
-            //proj.SetBuildProperty(targetGuid, "SWIFT_OBJC_BRIDGING_HEADER", "Libraries/Plugins/iOS/UnityIosPlugin/Source/UnityPlugin-Bridging-Header.h");
-            //proj.SetBuildProperty(targetGuid, "SWIFT_OBJC_INTERFACE_HEADER_NAME", "UnityIosPlugin-Swift.h");
+        //proj.SetBuildProperty(targetGuid, "SWIFT_OBJC_BRIDGING_HEADER", "Libraries/Plugins/iOS/UnityIosPlugin/Source/UnityPlugin-Bridging-Header.h");
+        //proj.SetBuildProperty(targetGuid, "SWIFT_OBJC_INTERFACE_HEADER_NAME", "UnityIosPlugin-Swift.h");
 
-            proj.AddBuildProperty(targetGuid, "LD_RUNPATH_SEARCH_PATHS", "@executable_path/Frameworks $(PROJECT_DIR)/lib/$(CONFIGURATION) $(inherited)");
-            proj.AddBuildProperty(targetGuid, "FRAMERWORK_SEARCH_PATHS",
-                "$(inherited) $(PROJECT_DIR) $(PROJECT_DIR)/Frameworks");
-            proj.AddBuildProperty(targetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
-            proj.AddBuildProperty(targetGuid, "DYLIB_INSTALL_NAME_BASE", "@rpath");
-            proj.AddBuildProperty(targetGuid, "LD_DYLIB_INSTALL_NAME",
-                "@executable_path/../Frameworks/$(EXECUTABLE_PATH)");
-            proj.AddBuildProperty(targetGuid, "DEFINES_MODULE", "YES");
-            proj.AddBuildProperty(targetGuid, "SWIFT_VERSION", "4.0");
-            proj.AddBuildProperty(targetGuid, "COREML_CODEGEN_LANGUAGE", "Swift");
+        proj.AddBuildProperty(targetGuid, "LD_RUNPATH_SEARCH_PATHS", "@executable_path/Frameworks $(PROJECT_DIR)/lib/$(CONFIGURATION) $(inherited)");
+        proj.AddBuildProperty(targetGuid, "FRAMERWORK_SEARCH_PATHS",
+            "$(inherited) $(PROJECT_DIR) $(PROJECT_DIR)/Frameworks");
+        proj.AddBuildProperty(targetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
+        proj.AddBuildProperty(targetGuid, "DYLIB_INSTALL_NAME_BASE", "@rpath");
+        proj.AddBuildProperty(targetGuid, "LD_DYLIB_INSTALL_NAME",
+            "@executable_path/../Frameworks/$(EXECUTABLE_PATH)");
+        proj.AddBuildProperty(targetGuid, "DEFINES_MODULE", "YES");
+        proj.AddBuildProperty(targetGuid, "SWIFT_VERSION", "4.0");
+        proj.AddBuildProperty(targetGuid, "COREML_CODEGEN_LANGUAGE", "Swift");
             
-            proj.WriteToFile(projPath);
-        }
+        proj.WriteToFile(projPath);
     }
 }
+#endif
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,16 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+### Changed
+- Added dependencies to manifest.json
+- Increase Android target Sdk version
+
+### Fixed
+- Fixed namespace name 'iOS'
+- Fixed namespace name 'Newtonsoft'
+
 
 ## [0.0.2] - 2023-02-28
 ### Fixed
