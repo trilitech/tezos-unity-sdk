21733e1790e99283cb106add11260d62af7423e4
Felipe Cardozo <fraifelipe@gmail.com>
2023-04-25 12:49:29 -0300

    Add custom readme (#42)
    * Add custom readme
    
    * Fix readme images;
    
    * Changelog
    
    ---------
    
    Co-authored-by: mismirnov <mi.smirnov24@gmail.com>

---
+++ Runtime/Scripts/ReadMe/Scripts/Runtime/ReadMeConstants.cs
@@ -0,0 +1,25 @@
+﻿
+namespace RMC.Core.ReadMe
+{
+	/// <summary>
+	/// Common values for <see cref="ReadMe"/>
+	/// </summary>
+	public static class ReadMeConstants
+	{
+		//  Properties ------------------------------------
+        
+		//  Fields ----------------------------------------
+        public const string MenuItemPathCreate = CompanyName + "/" + ProjectName;
+        public const string MenuItemPathWindow = "Window/" + CompanyName + "/" + ProjectName;
+
+		//
+		public const string CompanyName = "RMC";
+		public const string ProjectName = "ReadMe";
+		public const string Open = "Open";
+		
+		//
+		public const int MenuPriority_Primary = 0;		//put above divider line
+		public const int MenuPriority_Secondary = 100; //put below divider line
+
+	}
+}
