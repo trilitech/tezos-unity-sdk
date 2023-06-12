0799f80e75ab5dbfc321d862e8f897703586cd7f
umutkutlu <umut.1kutlu@gmail.com>
2023-06-12 22:17:32 +0300

    NFT icons added, scripts organized, IPFSImageDownloader added

---
+++ Samples~/Starter Sample/Scripts/IPFSImageDownloader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Tezos.StarterSample
+{
+    public class IPFSImageDownloader
+    {
+        public static IEnumerator DownloadAndSetImageTexture(string url, Image image)
+        {
+            if (url.StartsWith("ipfs:"))
+                url = url.Replace("ipfs://", "infura-ipfs.io/ipfs/");
+            url = url.Insert(0, "https://");
+            using (UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(url))
+            {
+                DownloadHandler handle = webRequest.downloadHandler;
+                yield return webRequest.SendWebRequest();
+
+                switch (webRequest.result)
+                {
+                    case UnityWebRequest.Result.ConnectionError:
+                    case UnityWebRequest.Result.DataProcessingError:
+                        throw new Exception(webRequest.error);
+                    case UnityWebRequest.Result.ProtocolError:
+                        throw new Exception("Http Error: " + webRequest.error);
+                    case UnityWebRequest.Result.Success:
+                        try
+                        {
+                            Texture2D texture2d = DownloadHandlerTexture.GetContent(webRequest);
+
+                            Sprite sprite = null;
+                            sprite = Sprite.Create(texture2d, new Rect(0, 0, texture2d.width, texture2d.height), UnityEngine.Vector2.zero);
+
+                            if (sprite != null)
+                            {
+                                image.sprite = sprite;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.LogError(e.Message);
+                        }
+
+                        break;
+                }
+            }
+        }
+    }
+}
--- Samples~/Starter Sample/Scripts/UINFTElement.cs
+++ Samples~/Starter Sample/Scripts/UINFTElement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -10,6 +11,17 @@ namespace Tezos.StarterSample
     {
         public string id { get; set; }
         public string amount { get; set; }
+        public ContractItem item { get; set; }
+    }
+    
+    public struct ContractItem
+    {
+        public string damage { get; set; }
+        public string armor { get; set; }
+        public string attackSpeed { get; set; }
+        public string healthPoints { get; set; }
+        public string manaPoints { get; set; }
+        public string itemType { get; set; }
     }
     
     public class UINFTElement : MonoBehaviour
@@ -18,12 +30,15 @@ namespace Tezos.StarterSample
         [SerializeField] private Image _nftImage;
         [SerializeField] private TextMeshProUGUI _idText;
         [SerializeField] private TextMeshProUGUI _amountText;
+        [SerializeField] private List<Sprite> _items;
         
         public void InitNFT(NFTData nftData)
         {
             //_nftImage.sprite = nftData.sprite;
             _idText.text = "ID: " + nftData.id;
             _amountText.text = "Amount: " + nftData.amount;
+            Debug.Log(nftData.item);
+            _nftImage.sprite = _items[Convert.ToInt32(nftData.id) == 0 ? 0 : Convert.ToInt32(nftData.item.itemType) % 4 + 1];
         }
     }
 }
