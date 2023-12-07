0e7106d01082692003e62fb7d19fbcc6ae5f21fc
Konst <johnysalvesen@gmail.com>
2023-12-07 20:48:59 +0300

    Removed unnecessary scripts from WalletConnection example scene; Added IPFS upload scene;

--- Runtime/Scripts/Tezos/TezosManager.cs
+++ Runtime/Scripts/Tezos/TezosManager.cs
@@ -16,20 +16,19 @@ namespace TezosSDK.Tezos
 		
 		[Tooltip("Logs will be printed to the console if the log level is equal or higher than this value.")]
 		[SerializeField] private Logger.LogLevel logLevel = Logger.LogLevel.Debug;
+		[Tooltip("Create API key in Pinata service https://app.pinata.cloud/developers/api-keys and paste JWT value " +
+		         "here to be able to upload images to IPFS.")]
+		[SerializeField] private string pinataApiKey;
 
 		public DAppMetadata DAppMetadata { get; private set; }
 
 		public ITezos Tezos { get; private set; }
-		
-		public IWalletProvider Wallet
-		{
-			get => Tezos?.Wallet;
-		}
 
-		public WalletEventManager MessageReceiver
-		{
-			get => Wallet?.EventManager;
-		}
+		public IWalletProvider Wallet => Tezos?.Wallet;
+
+		public WalletEventManager MessageReceiver => Wallet?.EventManager;
+
+		public static string PinataApiKey => TezosConfig.Instance.pinataApiKey;
 
 		protected override void Awake()
 		{
@@ -40,11 +39,10 @@ namespace TezosSDK.Tezos
 		private void InitializeTezos()
 		{
 			Logger.CurrentLogLevel = logLevel;
+			TezosConfig.Instance.pinataApiKey = pinataApiKey;
 			
 			DAppMetadata = new DAppMetadata(appName, appUrl, appIcon, appDescription);
 			Tezos = new Tezos(DAppMetadata);
 		}
 	}
-	
-
 }
