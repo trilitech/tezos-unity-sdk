ddc31acafcdf83b0c0af46b6a756d311fb8287db
Can Berk T <53381468+cnbrk13@users.noreply.github.com>
2024-03-22 13:06:32 +0000

    Refactored GetJsonCoroutine and PostJsonCoroutine to use HandleResponse method.

--- Runtime/Scripts/Helpers/HttpClients/TezosHttpClient.cs
+++ Runtime/Scripts/Helpers/HttpClients/TezosHttpClient.cs
@@ -62,66 +62,60 @@ namespace TezosSDK.Helpers.HttpClients
 			return url.EndsWith("/") ? url : $"{url}/";
 		}
 
-		protected IEnumerator GetJsonCoroutine<T>(string path, Action<Result<T>> callback = null)
+		private void HandleResponse<T>(UnityWebRequest request, Action<Result<T>> callback, out T result)
 		{
-			using var request = GetUnityWebRequest(UnityWebRequest.kHttpVerbGET, path);
-
-			yield return request.SendWebRequest();
+			result = default;
 
 			if (request.result != UnityWebRequest.Result.Success)
 			{
-				callback(new Result<T>(request.error));
-				yield break;
+				callback?.Invoke(new Result<T>(request.error));
 			}
+			else if (string.IsNullOrWhiteSpace(request.downloadHandler.text))
+			{
+				callback?.Invoke(new Result<T>("No data or empty JSON received."));
+			}
+			else
+			{
+				try
+				{
+					result = DeserializeJson<T>(request.downloadHandler.text);
+					callback?.Invoke(new Result<T>(result));
+				}
+				catch (Exception ex)
+				{
+					callback?.Invoke(new Result<T>(ex.Message));
+				}
+			}
+		}
 
-			if (string.IsNullOrWhiteSpace(request.downloadHandler.text))
-			{
-				callback(new Result<T>("No data or empty JSON received."));
-				yield break;
-			}
+		protected IEnumerator GetJsonCoroutine<T>(string path, Action<Result<T>> callback = null)
+		{
+			using var request = GetUnityWebRequest(UnityWebRequest.kHttpVerbGET, path);
+			Logger.LogDebug($"Request: {request.url}");
+			yield return request.SendWebRequest();
 
-			try
-			{
-				var result = DeserializeJson<T>(request.downloadHandler.text);
-				callback(new Result<T>(result));
-			}
-			catch (Exception ex)
-			{
-				callback(new Result<T>(ex.Message));
-			}
+			T result;
+			HandleResponse(request, callback, out result);
+			yield return result; // Yield the result now instead of yielding control to another coroutine
 		}
 
 		protected IEnumerator PostJsonCoroutine<T>(string path, object data, Action<Result<T>> callback = null)
 		{
 			var serializedData = JsonSerializer.Serialize(data, JsonOptions.DefaultOptions);
-
 			using var request = GetUnityWebRequest(UnityWebRequest.kHttpVerbPOST, path);
-
 			request.uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(serializedData));
 			request.SetRequestHeader(HttpHeaders.ContentType.Key, HttpHeaders.ContentType.Value);
 			yield return request.SendWebRequest();
 
-			if (request.result != UnityWebRequest.Result.Success)
-			{
-				callback(new Result<T>(request.error));
-				yield break;
-			}
-
-			try
-			{
-				var result = DeserializeJson<T>(request.downloadHandler.text);
-				callback(new Result<T>(result));
-			}
-			catch (Exception ex)
-			{
-				callback(new Result<T>(ex.Message));
-			}
+			T result;
+			HandleResponse(request, callback, out result);
+			yield return result; // Yield the result now instead of yielding control to another coroutine
 		}
 
 		private UnityWebRequest GetUnityWebRequest(string method, string path)
 		{
 			var request = new UnityWebRequest($"{BaseAddress}{path}", method);
-			Logger.LogDebug($"Sending {method} request to {request.url}");
+			Logger.LogDebug($"Preparing {method} request to {request.url}");
 			request.downloadHandler = new DownloadHandlerBuffer();
 			request.SetRequestHeader(HttpHeaders.Accept.Key, HttpHeaders.Accept.Value);
 			request.SetRequestHeader(HttpHeaders.UserAgent.Key, HttpHeaders.UserAgent.Value);
