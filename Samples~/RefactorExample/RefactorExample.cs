d9badaef005555f209f59572f87ac01d34cbf0f2
talha-trili <talha.isik@trili.tech>
2024-09-26 13:42:53 +0100

    fix: refactor example updated to reflect new changes

--- Samples~/RefactorExample/RefactorExample.cs
+++ Samples~/RefactorExample/RefactorExample.cs
@@ -1,54 +1,188 @@
-using TezosSDK.API;
-using TezosSDK.WalletProvider;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Netezos.Encoding;
+using Tezos.API;
+using Tezos.Operation;
+using Tezos.QR;
+using Tezos.SocialLoginProvider;
+using Tezos.WalletProvider;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class RefactorExample : MonoBehaviour
 {
-        [SerializeField] private TMP_Text _informationText;
-        [SerializeField] private Button   _connectButton;
-        [SerializeField] private Button   _disconnectButton;
-        [SerializeField] private Button   _logInButton;
-        [SerializeField] private Button   _logOutButton;
-        [SerializeField] private Button   _fetchBalanceButton;
-            
-        private void Awake()
-        {
-            _connectButton.onClick.AddListener(OnConnectClicked);
-            _disconnectButton.onClick.AddListener(OnDisconnectClicked);
-            _logInButton.onClick.AddListener(OnLogInClicked);
-            _logOutButton.onClick.AddListener(OnLogOutClicked);
-            _fetchBalanceButton.onClick.AddListener(OnFetchBalanceClicked);
-        }
+	[SerializeField] private TMP_Text        _informationText;
+	[SerializeField] private TMP_Text        _payloadText;
+	[SerializeField] private Button          _connectButton;
+	[SerializeField] private Button          _disconnectButton;
+	[SerializeField] private Button          _logInButton;
+	[SerializeField] private Button          _fetchBalanceButton;
+	[SerializeField] private Button          _signPayloadButton;
+	[SerializeField] private Button          _sendOperationButton;
+	[SerializeField] private QrCodeGenerator _qrCodeGenerator;
+	[SerializeField] private GameObject      _qrCodeContainer;
 
-        private async void OnFetchBalanceClicked()
-        {
-            int convertedBalance = (int)(await TezosAPI.GetXTZBalance() / 1000000);
-            _informationText.text = $"Balance: {convertedBalance}";
-        }
+	private async void Awake()
+	{
+		await TezosAPI.WaitUntilSDKInitialized();
 
-        private async void OnConnectClicked()
-        {
-            var result = await TezosAPI.ConnectWallet(new WalletProviderData { WalletType = WalletType.BEACON });
-            _informationText.text = $"{result.WalletAddress}";
-        }
+		TezosAPI.WalletConnected    += OnWalletConnected;
+		TezosAPI.WalletDisconnected += OnWalletDisconnected;
+		TezosAPI.PairingRequested   += OnPairingRequested;
 
-        private async void OnDisconnectClicked()
-        {
-            var result = await TezosAPI.DisconnectWallet();
-            _informationText.text = $"{result}";
-        }
-            
-        private async void OnLogInClicked()
-        {
-            var result = await TezosAPI.SocialLogIn(new SocialProviderData { SocialLoginType = SocialLoginType.Kukai });
-            _informationText.text = $"{result.WalletAddress}";
-        }
+		_connectButton.onClick.AddListener(OnConnectClicked);
+		_disconnectButton.onClick.AddListener(OnDisconnectClicked);
+		_logInButton.onClick.AddListener(OnLogInClicked);
+		_fetchBalanceButton.onClick.AddListener(OnFetchBalanceClicked);
+		_signPayloadButton.onClick.AddListener(OnSignPayloadClicked);
+		_sendOperationButton.onClick.AddListener(OnSendOperationClicked);
 
-        private async void OnLogOutClicked()
-        {
-            var result = await TezosAPI.SocialLogOut();
-            _informationText.text = $"{result}";
-        }
+		if (TezosAPI.IsConnected()) _informationText.text = TezosAPI.GetConnectionAddress();
+	}
+
+	private void OnWalletConnected(WalletProviderData walletProviderData) => _informationText.text = $"{walletProviderData.WalletAddress}";
+
+	private async void OnSendOperationClicked()
+	{
+		try
+		{
+			var parameter = new MichelinePrim
+			                {
+				                Prim = PrimType.Pair,
+				                Args = new List<IMicheline>
+				                       {
+					                       new MichelineInt(11000856),
+					                       new MichelinePrim
+					                       {
+						                       Prim = PrimType.Pair,
+						                       Args = new List<IMicheline>
+						                              {
+							                              new MichelineInt(1),
+							                              new MichelinePrim
+							                              {
+								                              Prim = PrimType.Pair,
+								                              Args = new List<IMicheline>
+								                                     {
+									                                     new MichelinePrim { Prim = PrimType.None },
+									                                     new MichelinePrim
+									                                     {
+										                                     Prim = PrimType.Pair,
+										                                     Args = new List<IMicheline>
+										                                            {
+											                                            new MichelinePrim { Prim = PrimType.None },
+											                                            new MichelineArray()
+										                                            }
+									                                     }
+								                                     }
+							                              }
+						                              }
+					                       }
+				                       }
+			                }.ToJson();
+
+			const string entryPoint  = "fulfill_ask";
+			const string destination = "KT1MFWsAXGUZ4gFkQnjByWjrrVtuQi4Tya8G";
+			const ulong  amount      = 1500000;
+
+			var req = new OperationRequest
+			          {
+				          Destination = destination,
+				          EntryPoint  = entryPoint,
+				          Arg         = parameter,
+				          Amount      = amount
+			          };
+
+			_payloadText.text = (await TezosAPI.RequestOperation(req)).TransactionHash;
+		}
+		catch (Exception e)
+		{
+			_payloadText.text = $"{e.Message}";
+		}
+	}
+
+	private void OnWalletDisconnected() => _informationText.text = $"Wallet disconnected";
+
+	private void OnPairingRequested(string pairingData)
+	{
+		_qrCodeContainer.SetActive(true);
+		_qrCodeGenerator.SetQrCode(pairingData);
+	}
+
+	private async void OnSignPayloadClicked()
+	{
+		try
+		{
+			var payload    = "Hello World!";
+			var bytes      = Encoding.UTF8.GetBytes(payload);
+			var hexPayload = BitConverter.ToString(bytes);
+			hexPayload = hexPayload.Replace("-", "");
+			hexPayload = "05" + hexPayload;
+			var result = await TezosAPI.RequestSignPayload(
+			                                               new SignPayloadRequest
+			                                               {
+				                                               Payload     = hexPayload,
+				                                               SigningType = SignPayloadType.MICHELINE
+			                                               }
+			                                              );
+			_payloadText.text = $"Signature: {result.Signature}";
+		}
+		catch (Exception e)
+		{
+			_payloadText.text = $"{e.Message}";
+		}
+	}
+
+	private async void OnFetchBalanceClicked()
+	{
+		try
+		{
+			int convertedBalance = (int)(await TezosAPI.GetXTZBalance() / 1000000);
+			_informationText.text = $"Balance: {convertedBalance}";
+		}
+		catch (Exception e)
+		{
+			_informationText.text = e.Message;
+		}
+	}
+
+	private async void OnConnectClicked()
+	{
+		try
+		{
+			var result = await TezosAPI.ConnectWallet(new WalletProviderData { WalletType = WalletType.BEACON });
+			OnWalletConnected(result);
+		}
+		catch (Exception e)
+		{
+			_informationText.text = e.Message;
+		}
+	}
+
+	private async void OnDisconnectClicked()
+	{
+		try
+		{
+			var result = await TezosAPI.Disconnect();
+			_informationText.text = $"{result}";
+		}
+		catch (Exception e)
+		{
+			_informationText.text = e.Message;
+		}
+	}
+
+	private async void OnLogInClicked()
+	{
+		try
+		{
+			var result = await TezosAPI.SocialLogIn(new SocialProviderData { SocialLoginType = SocialLoginType.Kukai });
+			_informationText.text = $"{result.WalletAddress}";
+		}
+		catch (Exception e)
+		{
+			_informationText.text = e.Message;
+		}
+	}
 }
