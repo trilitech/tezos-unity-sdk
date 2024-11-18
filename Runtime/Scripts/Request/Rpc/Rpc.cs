e8093f54e262c8d8238eea191781f6921653d32e
talha-trili <talha.isik@trili.tech>
2024-11-18 12:31:08 +0000

    chore: unused method and namings removed

--- Runtime/Scripts/Request/Rpc/Rpc.cs
+++ Runtime/Scripts/Request/Rpc/Rpc.cs
@@ -1,13 +1,9 @@
-﻿using Tezos.Cysharp.Threading.Tasks;
-
-namespace Tezos.Request
+﻿namespace Tezos.Request
 {
 	public class Rpc : TezosClient
 	{
 		public Rpc(int timeOut) : base(timeOut)
 		{
 		}
-
-		public UniTask<T> GetContractCode<T>(string contract) => GetRequest<T>($"chains/main/blocks/head/context/contracts/{contract}/script/");
 	}
 }
